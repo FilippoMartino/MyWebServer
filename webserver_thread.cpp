ac16951b567383b162979850be6450e2be2821da
filippomartino <filippo.martino@itiscuneo.eu>
2019-03-15 16:38:00 +0100

    Aggiunta di nuove funzioni di lettura file, da fixare martedì

--- webserver_thread.cpp
+++ webserver_thread.cpp
@@ -3,14 +3,10 @@
 #define HOME "HTTP/1.1 200 OK\n\
 Date: Wed, 19 Apr 2017 16:34:20 GMT\n\
 Content-Type: text/html; charset=UTF-8\n\
-\n <!DOCTYPE html>\n\
-<html class=\"client-nojs\" lang=\"it\" dir=\"ltr\">\n\
-<head>\n\
-<title>web site</title>\n\
-<body>\n\
-<h1>Hello world</h1>\n\
-</body>\n\
-</html>"
+"
+
+#define ERROR_FILE_MISSING "Error 404: file not found"
+#define MAXCHAR 1000
 
 typedef struct {
 
@@ -19,6 +15,58 @@ typedef struct {
 		ServerTcp* myself;
 
 } Params;
+
+int get_file_size(char* filename){
+
+	FILE *fp;
+	long lSize;
+
+	fp = fopen(filename, "r");
+  if (fp == NULL){
+      printf("An error has ecoured when opening the file\n");
+  }
+
+	fseek (fp , 0 , SEEK_END);
+	lSize = ftell(fp);
+
+	return lSize;
+
+}
+
+char* read_file(char* filename){
+
+	FILE *fp;
+	long lSize;
+	char * buffer;
+	size_t result;
+
+  fp = fopen(filename, "r");
+  if (fp == NULL){
+      printf("An error has ecoured when opening the file\n");
+  }
+
+	// calcolo la lunghezza del file
+	fseek (fp , 0 , SEEK_END);
+	lSize = ftell(fp);
+
+	// torniamo all'inizio del file
+	rewind(fp);
+
+	// alloco la memoria per contenere l'intero file
+	buffer = (char*) malloc ( sizeof ( char ) * lSize );
+
+	// copio il file nel buffer
+	result = fread (buffer, 1, lSize, fp);
+
+	//Se il file non esiste
+	if (result != lSize)
+		return strdup("Error");
+
+	fclose (fp);
+
+	return buffer;
+
+}
 
 void* request(void* params) {
 
@@ -35,8 +83,17 @@ void* request(void* params) {
   // Ricevo le richieste
   char* richiesta_client = conn->receive_message();
   // Invio le risposte
-  conn->send_message(home);
-		// Disconnetto questa conn dalla lista
+
+	long lSize = get_file_size("index.html");
+	char* page = (char*) malloc ( sizeof ( char ) * lSize );
+
+
+	page = read_file("index.html");
+	printf("page ->%s\n", page);
+
+	conn->send_message(page);
+	printf("Pagina inviata\n");
+	// Disconnetto questa conn dalla lista
   myself->disconnect(conn);
   // Termino il thread
   pthread_exit(NULL);
@@ -47,7 +104,7 @@ int main (int argc, char const *argv[]){
 
   int port = atoi(argv[1]);
 
-		// Idea di Wang, far diventare questo un SINGLETON (DP GOF)
+	// Idea di Wang, far diventare questo un SINGLETON (DP GOF)
   ServerTcp* myself = new ServerTcp(port);
 
   while(true){
